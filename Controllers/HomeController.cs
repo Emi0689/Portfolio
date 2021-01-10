53c6e9aa3823043971ca80fe031370c64b3465ae
Emi <emiliofilotti@gmail.com>
2021-01-10 17:10:32 -0300

    Subida version beta

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Portfolio.Controllers
@@ -21,6 +24,32 @@ namespace Portfolio.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public void SendReply(string Contact_Name, string Contact_Email, string Contact_Subject, string Contact_Text)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress(Contact_Email);
+                mail.To.Add(Constantes.emailenvio.ToString());
+                mail.Subject = "Portfolio Mio " + Contact_Subject;
+                mail.Body = "<html><body>"+ Contact_Name + " <br /> " + Contact_Text;
+                mail.IsBodyHtml = true;
+                SmtpServer.UseDefaultCredentials = false;
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new NetworkCredential(Constantes.emailenvio.ToString(), Constantes.credencialenvio.ToString());
+                SmtpServer.EnableSsl = true;
+                SmtpServer.Send(mail);
+
+                SmtpServer.Dispose();
+            }
+            catch (Exception ex)
+            {
+            }
         }
 
         public IActionResult Privacy()
